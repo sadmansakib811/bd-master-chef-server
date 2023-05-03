b5902a6baeb06a9872caf6ca02f7bc4ef31cfb4e
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-03 03:23:02 +0200

    dynamic api link created using filter

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(cors())
 const port = process.env.PORT || 5000
 
 const chef = require('./chef.json')
+const ranna = require('./ranna.json')
 app.get('/', (req, res) => {
   res.send('chef is cooking')
 })
@@ -12,6 +13,24 @@ app.get('/', (req, res) => {
 app.get('/chef', (req, res) => {
     res.send(chef)
   })
+
+  app.get('/ranna', (req, res) => {
+    res.send(ranna)
+  })
+
+  app.get('/chef/:id',(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    if(id == 0){
+      res.send(ranna);
+    }
+    else{
+      const selectedChefsAllRanna = ranna.filter( n=> n.chef_id == id );
+    res.send(selectedChefsAllRanna);
+    }
+    
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
